7d897d027e23df51f792287e1c8e526c14440523
Joao Carlos <thesilentwarrior@gmail.com>
2017-08-30 21:37:38 +0100

    initial commit

---
+++ MissionEditor/data/scripts/UnitPayloads/MiG-29A.lua
@@ -0,0 +1,51 @@
+local unitPayloads = {
+	["name"] = "MiG-29A",
+	["payloads"] = {
+		[1] = {
+			["name"] = "1 - ER, 73, No Radar Needed",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{B79C379A-9E87-4E50-A1EE-7F7E29C2E87A}",
+					["num"] = 3,
+				},
+				[2] = {
+					["CLSID"] = "{2BEC576B-CDF5-4B7F-961F-B0FA4312B841}",
+					["num"] = 4,
+				},
+				[3] = {
+					["CLSID"] = "{B79C379A-9E87-4E50-A1EE-7F7E29C2E87A}",
+					["num"] = 5,
+				},
+				[4] = {
+					["CLSID"] = "{FBC29BFE-3D24-4C64-B81D-941239D12249}",
+					["num"] = 7,
+				},
+				[5] = {
+					["CLSID"] = "{FBC29BFE-3D24-4C64-B81D-941239D12249}",
+					["num"] = 6,
+				},
+				[6] = {
+					["CLSID"] = "{FBC29BFE-3D24-4C64-B81D-941239D12249}",
+					["num"] = 2,
+				},
+				[7] = {
+					["CLSID"] = "{FBC29BFE-3D24-4C64-B81D-941239D12249}",
+					["num"] = 1,
+				},
+			},
+			["tasks"] = {
+				[1] = 18,
+				[2] = 19,
+				[3] = 10,
+				[4] = 11,
+				[5] = 32,
+				[6] = 34,
+				[7] = 16,
+				[8] = 30,
+				[9] = 31,
+			},
+		},
+	},
+	["unitType"] = "MiG-29A",
+}
+return unitPayloads
